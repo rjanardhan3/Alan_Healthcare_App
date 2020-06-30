4332a118213e10ed3c47f76db12f69d7e2f2b9cd
rajbrady <raj@alan.app>
2020-06-30 14:24:10 -0700

    Initial Commit

---
+++ Alan_Health_Care_Discovery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alan_Health_Care_Discovery
+//
+//  Created by Raj Janardhan on 6/30/20.
+//  Copyright © 2020 Raj Janardhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
